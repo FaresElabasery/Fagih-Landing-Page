8d33e6d5be0fef923b3782f0069e483b341bb426
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2025-12-24 09:51:24 +0200

    update : url for meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
     {
-      url: "https://fagih-landing-page.vercel.app/og-image.png",
+      url: "https://fagih-landing-page.vercel.app/images/og-image.png",
       width: 1200,
       height: 630,
       alt: "مكتب حسن محمد فقيه للاستشارات الهندسية",
